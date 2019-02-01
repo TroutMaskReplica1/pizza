57993e465620a067a7191660040e4d839a0b3c9d
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-02-01 10:56:24 -0800

    add start of business logic, following correct build steps now

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,32 @@ function Pizza(size, meat, veggie) {
   this.meat = meat;
   this.veggie = veggie;
 }
+
+Pizza.prototype.toppingPrice = function() {
+  var size = $("#size").val();
+  if (size === "small") {
+    for (i=0; i > toppings.length; i++)
+    return 0.5;
+  } else if (size === "medium") {
+    for (i=0; i > toppings.length; i++)
+    return 1;
+  } else if (size === "large") {
+    for (i=0; i > toppings.length; i++)
+    return 1.5;
+  }
+}
+
+var piz = new Pizza("small", "pepperoni", "olive");
+
+
+// $(document).ready(function() {
+//   $("#sub").click(function() {
+//     var size = $("#size").val();
+//     var meatInput = $("#meat:checked").val();
+//     var veggieInput = $("input[type=checkbox]:checked").val();
+//     var meat = [];
+//     var veggie = []
+//     $(".display").text("You ordered a "  + size + " " + meat + " " + veggie );
+//     event.preventDefault();
+//   });
+// });
