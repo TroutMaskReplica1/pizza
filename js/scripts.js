c28e129aad9836d99c453bbab825fd3d081996a0
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-02-08 11:01:14 -0800

    fix it

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,51 @@
 function Pizza(size, meat, veggie) {
   this.size = size;
-  this.topping = [];
+  this.meat = [];
+  this.veggie = [];
   this.price = 0;
 }
 
 Pizza.prototype.sizePrice = function() {
   var size = $("#size").val();
   if (size === "small") {
-    return 5;
+    this.price += 5;
   } else if (size === "medium") {
-    return 7;
+    this.price += 7;
   } else if (size === "large") {
-    return 9;
+    this.price += 9;
   }
 }
 
-Pizza.prototype.toppingPrice = function() {
-  for (i=0; i<this.topping.length; i++)
-      this.price + 1;
+Pizza.prototype.meatPrice = function() {
+  if ($("input:radio[name=meat]:checked")) {
+    this.price += 1;
+      }
     }
 
-var piz = new Pizza();
+Pizza.prototype.veggiePrice = function() {
+  if ($("input:radio[name=veggie]:checked")) {
+    this.price += 0.5;
+    }
+  }
+
 
 $(document).ready(function() {
   $("#pizza").submit(function() {
     event.preventDefault();
+    var piz = new Pizza();
     var size = $("#size").val();
-    var topping = $("input[name=topping]:checked").val();
-    function topPush() {
-      return piz.topping.push(this).value;
+    var meat = $("input:radio[name=meat]:checked").val();
+    var veggie = $("input:radio[name=veggie]:checked").val();
+    function meatPush() {
+      return (piz.meat).push(meat);
     }
-    topPush(piz.topping);
-    $(".display").text("You ordered a " + piz.topping + " pizza");
-    $("#price").text("That will be $" + piz.sizePrice() );
+    function veggiePush() {
+      return (piz.veggie).push(veggie);
+    }
+    meatPush(meat);
+    veggiePush(veggie);
+    price = parseInt(piz.sizePrice() + piz.meatPrice() + piz.veggiePrice());
+    $(".display").text("You ordered a " + piz.meat + " " + piz.veggie + " pizza");
+    $("#price").text("That will be $" + piz.price );
   });
 });
